a2448289c2156f665cecbec218fc693036bea4b3
ethanhousley1 <ethanhousley@gmail.com>
2025-04-12 13:00:45 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+document.querySelectorAll('.clickable').forEach((div) => {
+    div.addEventListener('click', function(event) {
+        let clickedDiv = event.currentTarget;
+        speakerClicked(clickedDiv);
+    });
+});
+document.body.addEventListener('hover', function (event) {
+
+})
+
+function speakerClicked(clickedDiv) {
+    let img = clickedDiv.querySelector('img');
+    img.classList.add('active-img')
+    let cardTitle = clickedDiv.querySelector('.card-title');
+    let clickedOnce = clickedDiv.dataset.clicked === 'true';
+    cardTitle.classList.add('card-title-active');
+    if (!clickedOnce) {
+        clickedDiv.dataset.clicked = 'true';
+    } else {
+        clickedDiv.dataset.clicked = 'false';
+        goToLink(clickedDiv.dataset.label);
+    }
+    clickedDiv.addEventListener('mouseleave', function() {
+        clickedDiv.dataset.clicked = 'false';
+        console.log('Mouse left the div!');
+        cardTitle.classList.remove('card-title-active');
+        img.classList.remove('active-img');
+    })
+}
+
+function goToLink(link) {
+    window.open(link, '_blank');
+}
